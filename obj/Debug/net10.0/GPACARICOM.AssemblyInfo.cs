8c01848b3d65ac479be49ed5758faf444b5734e7
rahiemesamuels <rahiemesamuels121@gmail.com>
2026-06-25 16:58:28 -0500

    after curacoa lost

--- obj/Debug/net10.0/GPACARICOM.AssemblyInfo.cs
+++ obj/Debug/net10.0/GPACARICOM.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GPACARICOM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8520037a01e0bcff43b5c2bd7f3aeb59336df5e2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d771e77bad6fe5e5f135703312771b8cd56d4987")]
 [assembly: System.Reflection.AssemblyProductAttribute("GPACARICOM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GPACARICOM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
