517cf0c665fe4944cff112cebe2457761278b9e5
rahiemesamuels121 <rahiemesamuels121@gmail.com>
2026-06-30 22:10:42 -0500

    Changed Logo to the original and changed Dimensions

--- obj/Debug/net10.0/GPACARICOM.AssemblyInfo.cs
+++ obj/Debug/net10.0/GPACARICOM.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GPACARICOM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c01848b3d65ac479be49ed5758faf444b5734e7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a73e08d8b47ab160cf2ad51ddf448e1c63e61723")]
 [assembly: System.Reflection.AssemblyProductAttribute("GPACARICOM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GPACARICOM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
